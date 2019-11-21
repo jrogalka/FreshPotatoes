f46285d2a425c941f78a7c77aff2c2d7a5a2e7b6
Joel Rogalka <jrogalka@outlook.com>
2019-11-20 20:27:35 -0600

    update register and login

--- login.php
+++ login.php
@@ -9,16 +9,9 @@
         $password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
 
 
-        /*
-            In the following query, I will use the COLLATE function to stop the query from converting all characters to lowercase.
-            Wihtout this, the query would produce the following:
-            Input: UserName, PassWord
-            Value: username, password
-
-            This can also be done at the database level in phpmyadmin.
-        */
+        
         $query = "SELECT * FROM users
-                  WHERE Username = :username COLLATE Latin1_General_CS_AS && Password = :password COLLATE Latin1_General_CS_AS ";
+                  WHERE Username = :username && Password = :password";
         $statement = $db->prepare($query);
         $statement->bindValue(':username', $username);
         $statement->bindValue(':password', $password);
@@ -27,10 +20,8 @@
 
         //Correct User
         if ($statement->rowCount() >= 1) {
-            session_start();
             $_SESSION['UserId'] = $user['UserID'];
             $_SESSION['Username'] = $user['Username'];
-            $_SESSION['Password'] = $user['Password'];
             $_SESSION['Role'] = $user['Role'];
             header('Location: index.php');
         }
--- register.php
+++ register.php
@@ -1,80 +1,96 @@
 <?php
     include 'connect.php';
+    if (isset($_POST['submit'])) {
+        $username = $_POST['username'];
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = $_POST['password'];
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password2 = $_POST['password2'];
+        $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = $_POST['email'];
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+        $firstname = $_POST['firstname'];
+        $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $lastname = $_POST['lastname'];
+        $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    
-    $username = $_POST['username'];
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-    $password = $_POST['password'];
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
-    $password2 = $_POST['password2'];
-    $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = $_POST['email'];
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
-    $firstname = $_POST['firstname'];
-    $firstname = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_SPECIAL_CHARS);
-    $lastname = $_POST['lastname'];
-    $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $existsQuery = "SELECT * FROM users
+                        WHERE Username = :username";
+        $existsStatement = $db->prepare($existsQuery);
+        $existsStatement->bindValue(':username', $username);
+        $existsStatement->execute();
 
-    $existsQuery = "SELECT * FROM users
-                    WHERE Username = :username";
-    $existsStatement = $db->prepare($existsQuery);
-    $existsStatement->bindValue(':username', $username);
-    $existsStatement->execute();
-
-    if ($existsStatement->rowCount() > 0) {
-        echo '<script language="javascript">';
-        echo 'alert("Username Already Exists.");';
-        echo 'window.location.href = "register.php";';
-        echo '</script>';
-    }
-    else {
-        if (strlen($username) < 1) {
+        //Check if user exists
+        if ($existsStatement->rowCount() > 0) {
             echo '<script language="javascript">';
-            echo 'alert("Username must have at least 1 character.");';
-            echo 'window.location.href = "register.php";';
-            echo '</script>';
-        }
-        if (strlen($password) < 1) {
-            echo '<script language="javascript">';
-            echo 'alert("Password must have at least 1 character.");';
-            echo 'window.location.href = "register.php";';
-            echo '</script>';
-        }
-        if ($password != $password2) {
-            echo '<script language="javascript">';
-            echo 'alert("Passwords must match.");';
-            echo 'window.location.href = "register.php";';
-            echo '</script>';
-        }
-        if (strlen($email) < 1) {
-            echo '<script language="javascript">';
-            echo 'alert("Email must have at least 1 character.");';
-            echo 'window.location.href = "register.php";';
-            echo '</script>';
-        }
-        if (strlen($firstname) < 1) {
-            echo '<script language="javascript">';
-            echo 'alert("First name must have at least 1 character.");';
-            echo 'window.location.href = "register.php";';
-            echo '</script>';
-        }
-        if (strlen($lastname) < 1) {
-            echo '<script language="javascript">';
-            echo 'alert("Last name must have at least 1 character.");';
-            echo 'window.location.href = "register.php";';
+            echo 'alert("Username Already Exists.");';
+            echo 'window.location.href = "register.html";';
             echo '</script>';
         }
         else {
-            $query = "INSERT INTO users (Username, Password, Email, FirstName, LastName) VALUES (:username, :password, :email, :firstname, :lastname)";
-            $statement = $db->prepare($query);
-            $statement->bindValue(':username', $username);
-            $statement->bindValue(':password', $password);
-            $statement->bindValue(':email', $email);
-            $statement->bindValue(':firstname', $firstname);
-            $statement->bindValue(':lastname', $lastname);
-            $statement->execute();
+            //Ensure data is entered. 
+            if (strlen($username) < 1) {
+                echo '<script language="javascript">';
+                echo 'alert("Username must have at least 1 character.");';
+                echo 'window.location.href = "register.html";';
+                echo '</script>';
+            }
+            if (strlen($password) < 1) {
+                echo '<script language="javascript">';
+                echo 'alert("Password must have at least 1 character.");';
+                echo 'window.location.href = "register.html";';
+                echo '</script>';
+            }
+            if ($password != $password2) {
+                echo '<script language="javascript">';
+                echo 'alert("Passwords must match.");';
+                echo 'window.location.href = "register.html";';
+                echo '</script>';
+            }
+            if (strlen($email) < 1) {
+                echo '<script language="javascript">';
+                echo 'alert("Email must have at least 1 character.");';
+                echo 'window.location.href = "register.html";';
+                echo '</script>';
+            }
+            if (strlen($firstname) < 1) {
+                echo '<script language="javascript">';
+                echo 'alert("First name must have at least 1 character.");';
+                echo 'window.location.href = "register.html";';
+                echo '</script>';
+            }
+            if (strlen($lastname) < 1) {
+                echo '<script language="javascript">';
+                echo 'alert("Last name must have at least 1 character.");';
+                echo 'window.location.href = "register.hmtl";';
+                echo '</script>';
+            }
+            else {
+                $query = "INSERT INTO users (Username, Password, Email, FirstName, LastName) VALUES (:username, :password, :email, :firstname, :lastname)";
+                $statement = $db->prepare($query);
+                $statement->bindValue(':username', $username);
+                $statement->bindValue(':password', $password);
+                $statement->bindValue(':email', $email);
+                $statement->bindValue(':firstname', $firstname);
+                $statement->bindValue(':lastname', $lastname);
+                $statement->execute();
 
-            $UserID = $db->lastInsertId();
+                $UserID = $db->lastInsertId();
+                
+                $_SESSION['UserId'] = $UserID;
+                $_SESSION['Username'] = $username;
+                $_SESSION['Password'] = $password;
+                $_SESSION['Role'] = 0;
+
+                header('Location: index.php');
+            }
         }
     }
+    else {
+        echo '<script language="javascript">';
+        echo 'alert("This page cannot be accessed.");';
+        echo 'window.location.href = "index.php";';
+        echo '</script>';
+    }
+        
 ?>
